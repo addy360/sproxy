23ec80919cc5bd2bddf1f83c3b9a8554b69dfb72
addy360 <abbyboy360@gmail.com>
2022-07-28 15:08:12 +0300

    🤸‍♀️

--- src/main/java/com/addy360/proxyserver/utils/PageRequester.java
+++ src/main/java/com/addy360/proxyserver/utils/PageRequester.java
@@ -1,16 +1,21 @@
 package com.addy360.proxyserver.utils;
 
+import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class PageRequester {
 
     private Document getPage(String url) {
 
         try {
-            return Jsoup.connect(url).followRedirects(true).get();
+            return Jsoup.connect(url)
+                    .followRedirects(true)
+                    .get();
         } catch (Exception e) {
             return null;
         }
@@ -19,6 +24,9 @@ public class PageRequester {
     public String loadDocument(String url) {
         Document page = getPage(url);
         if (page == null) return "Error while fetching page.";
+        Elements src = page.getElementsByAttribute("src");
+        log.info("Base url : {}", page.baseUri());
+        log.info("src : {}", src);
         return page.toString();
     }
 
