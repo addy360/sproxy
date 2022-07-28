68b8c37853435b28a81caf5a696cd9d78eb1a329
addy360 <abbyboy360@gmail.com>
2022-07-28 11:34:31 +0300

    initial commit

---
+++ src/test/java/com/addy360/proxyserver/ProxyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.addy360.proxyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
